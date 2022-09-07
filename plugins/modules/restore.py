866396a3da34e0fab388aaffc3692ec51ac36cb2
Mike Graves <mgraves@redhat.com>
2022-09-07 09:02:34 -0400

    Use correct structure for state

--- plugins/modules/restore.py
+++ plugins/modules/restore.py
@@ -14,18 +14,18 @@ ARG_SPEC = {
 def find_by_arn(arn, state):
     for k, v in state.items():
         if v.get("Properties", {}).get("Arn") == arn:
-            return v
+            return k, v
 
 def main():
     module = AnsibleModule(argument_spec=ARG_SPEC)
     conn = module.params.get("connection") or {}
     client = AwsClient(**conn)
     event = module.params.get("event")
-    resource = find_by_arn(event["Arn"], module.params.get("current_state", {}))
+    name, resource = find_by_arn(event["Arn"], module.params.get("current_state", {}))
     if not resource:
         module.fail_json(changed=False, msg="Could not find resource")
     result = client.present(resource)
-    module.exit_json(changed=True, resources=result)
+    module.exit_json(changed=True, resources={name: result})
 
 
 if __name__ == "__main__":
